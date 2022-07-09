066d4a919a11b24951faaab311a60ca15c5cabc1
Oxana Garascenco <oxana.garashcenco@gmail.com>
2022-07-09 13:08:47 +0300

    removed test file, added main page component and place component, passed prop

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  placesNumber: number;
+}
+
+function App({placesNumber}: AppProps): JSX.Element {
+  return <MainPage placesNumber={placesNumber}/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Settings = {
+  PLACES_NUMBER: 5
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App placesNumber = {Settings.PLACES_NUMBER}/>
   </React.StrictMode>,
 );
